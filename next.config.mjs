101afa8d6ed3736a7d1739ddb6473f9b37119320
Shyam Sharki <ssk66221@gmail.com>
2024-08-09 23:38:48 +0545

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+
+    images:{
+        remotePatterns:[
+            {
+                protocol:'https',
+                hostname:'jobportal.com',
+                port:'',
+                pathname:'',
+            }
+        ]
+    }
+};
 
 export default nextConfig;
